d7d98face9b5bf52d4900e08bee877c7b67abacf
Jukka Pirinen <jukka@bogey@localhost>
2023-11-25 02:06:36 +0200

    Changed SCAN_INTERVAL and BAUDRATE

--- custom_components/kamstrup_403/const.py
+++ custom_components/kamstrup_403/const.py
@@ -11,8 +11,8 @@ ATTRIBUTION: Final = "Data provided by Kamstrup meter"
 
 # Defaults
 DEFAULT_NAME: Final = "kamstrup" # Used for sensor name prefix
-DEFAULT_BAUDRATE: Final = 1200
-DEFAULT_SCAN_INTERVAL: Final = 3600
+DEFAULT_BAUDRATE: Final = 9600
+DEFAULT_SCAN_INTERVAL: Final = 60
 DEFAULT_TIMEOUT: Final = 1.0
 DEFAULT_DEVICE_MODEL: Final = "403"
 
